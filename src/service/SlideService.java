0b136161689835d435ee31d090b81fc8e4231ef3
gtapiero <giovanny.tapiero@pi.edu.co>
2026-01-07 19:31:43 -0500

    commit - SlideService . / .

---
+++ src/service/SlideService.java
@@ -0,0 +1,14 @@
+package service;
+
+import dao.SlideDAO;
+import model.Slide;
+
+public class SlideService {
+
+    private final SlideDAO dao = new SlideDAO();
+
+    public void createSlide(String title, String data) throws Exception{
+        Slide slide = new Slide(title, data);
+        dao.save(slide);
+    }
+}   
